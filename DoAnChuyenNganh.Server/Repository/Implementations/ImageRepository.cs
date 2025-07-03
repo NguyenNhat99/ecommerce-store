39a25f7a83e44ddf07619452a9346e3aeaffdb63
NguyenHoangNhat <nguyennhat025.go@gmail.com>
2025-07-03 17:01:05 +0700

    Add Product and Image repository, Add automapper, Insert table data - Add Repository Product, Image - Register repository Product, Image - Add ProductImage, ProductResponse, ProductRequest, ProductSize - Insert table ProductSize, Size

---
+++ DoAnChuyenNganh.Server/Repository/Implementations/ImageRepository.cs
@@ -0,0 +1,43 @@
+﻿using DoAnChuyenNganh.Server.Data;
+using DoAnChuyenNganh.Server.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoAnChuyenNganh.Server.Repository.Implementations
+{
+    public class ImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly EcommerceStoreContext _context;
+
+        public ImageRepository(IWebHostEnvironment webHostEnvironment, EcommerceStoreContext context) 
+        {
+            _webHostEnvironment = webHostEnvironment;
+            _context = context;
+        }
+        public async Task<string> AddAsync(IFormFile file, string folder)
+        {
+            if (file != null)
+            {
+                var ext = Path.GetExtension(file.FileName);
+                var newFileName = $"{Guid.NewGuid()}{ext}";
+
+                var rootPath = _webHostEnvironment.ContentRootPath;
+                var folderPath = Path.Combine(rootPath, "Assets", folder);
+
+                var filePath = Path.Combine(folderPath, newFileName);
+
+                using var stream = new FileStream(filePath, FileMode.Create);
+                await file.CopyToAsync(stream);
+
+                return newFileName;
+            }
+            return "";
+        }
+        public async Task<string> GetByIdProductAsync(int idProduct)
+        {
+            var image =await _context.ProductImages.FirstOrDefaultAsync(p => p.Id == idProduct);
+            if (image == null) return "";
+            return image.ImageUrl;
+        }
+    }
+}
--- DoAnChuyenNganh.Server/Repository/RepositoryRegistration.cs
+++ DoAnChuyenNganh.Server/Repository/RepositoryRegistration.cs
@@ -10,6 +10,8 @@ namespace DoAnChuyenNganh.Server.Repository
         {
             services.AddScoped<IBrandRepository, BrandRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IImageRepository, ImageRepository>();
         }
     }
 }
